95baf0fb5f78eeca54b1b136d7f5c8ef1ace8d06
JD <john.c.doyle@gmail.com>
2023-05-21 18:05:41 -0400

    Change to stringify the call out to the API Gateway

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  fetch('https://api.gitmosaic.com/commit', {method: 'POST', body: github.context.payload});
+  fetch('https://api.gitmosaic.com/commit', {method: 'POST', body: JSON.stringify(github.context.payload)});
 } catch (error) {
   core.setFailed(error.message);
 }
